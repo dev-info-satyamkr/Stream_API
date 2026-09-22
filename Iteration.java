c1eec8cc5a058bfd94c159736739dee5d327bfab
dev-info-satyamkr <sy250957@gmail.com>
2026-09-22 21:41:12 +0530

    Iteration in Java

--- Iteration.java
+++ Iteration.java
@@ -2,6 +2,7 @@ package StreamAPI;
 
 import java.util.List;
 import java.util.Arrays;
+import java.util.Iterator;
 
 public class Iteration 
 {
@@ -9,13 +10,21 @@ public class Iteration
    {
     List<Integer> Values =Arrays.asList(1,2,3,4,5,6);
 
-    for(int i=0;i<6;i++)
+  //   for(int i=0;i<6;i++)
+  //   {
+  //     System.out.println(Values.get(i));
+  //       }    
+  //  }    
+  //   Iterator<Integer> i = Values.iterator();
+  //   while(i.hasNext())
+  //   {
+  //     System.out.println(i.next());
+  //   }
+
+  //  }
+    for(int i : Values)
     {
-      System.out.println(Values.get(i));
-        }    
-   }    
+      System.out.println(i);
+    }
+  }
 }
-
-
-
-
