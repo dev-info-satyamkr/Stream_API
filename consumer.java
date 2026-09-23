90013dc7dcf91b109bdec0f777fbb895c0829dac
dev-info-satyamkr <sy250957@gmail.com>
2026-09-23 13:46:28 +0530

    Stream Api Features part 3 Consumer Interface

---
+++ consumer.java
@@ -0,0 +1,22 @@
+package StreamAPI;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class consumer
+ {
+   public static void main(String[] args)
+   {
+    List<Integer> values = Arrays.asList(1,2,3,4,5,6);
+
+    Consumer<Integer> c = new Consumer<Integer>()
+    {
+        public void accept(Integer i)
+        {
+            System.out.println(i);
+        }
+    };
+      values.forEach(c);
+   } 
+}
